516922edf9b1c5b685b93172a31819bacd4b9d39
jtleider <jtleider@gmail.com>
2018-07-05 16:00:52 -0500

    Modify report across departments so it is interactive

--- crossDeptReport.py
+++ crossDeptReport.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import DataTable, TableColumn, NumberFormatter
+from bokeh.models.widgets import DataTable, TableColumn, NumberFormatter, Select, Div
+from bokeh.layouts import layout
 from bokeh.io import curdoc
 
 def gini(y):
@@ -21,46 +22,53 @@ assert abs(gini(pd.Series([0, 100])) == 0.5)
 assert abs(gini(pd.Series([998000, 20000, 17500, 70000, 23500, 45200])) - .7202) < .00005 # http://www.peterrosenmai.com/lorenz-curve-graphing-tool-and-gini-coefficient-calculator?
 assert abs(gini(pd.Series([1, 1, 2, 2])) - .167) < .0005 # http://shlegeris.com/gini
 
-def crossDeptReport(salaries, var='newsalaryperfte', varlabel='Salary', showFigure=False):
-	"""Generate report by department (within college).
-
-	Args:
-		salaries: pandas DataFrame with salary data.
-		var (str): Variable on which to produce report.
-		varlabel (str): Label for variable on which to produce report (for figure).
-		showFigure (bool): Plot bar chart of salaries.
-
-	Returns:
-		pandas DataFrame with report across departments.
-	"""
-	d = salaries.groupby(['campus', 'college', 'dept'])['newsalaryperfte'].agg(['size', 'count', 'min', 'mean', 'max', gini])
-	if showFigure:
-		source = ColumnDataSource(d)
-		# data table
-	return d
-
-
 salaries = pd.read_csv('salaries.csv', index_col=0)
-d = crossDeptReport(salaries, showFigure=True)
-d.reset_index(inplace=True)
-print(d)
 
-columns = [
-	TableColumn(field='campus', title='Campus'),
-	TableColumn(field='college', title='College'),
-	TableColumn(field='dept', title='Department'),
-	TableColumn(field='size', title='# of Employees'),
-	TableColumn(field='count', title='# with Data'),
-	TableColumn(field='min', title='Min', formatter=NumberFormatter(format="$0,0")),
-	TableColumn(field='mean', title='Mean', formatter=NumberFormatter(format="$0,0")),
-	TableColumn(field='max', title='Max', formatter=NumberFormatter(format="$0,0")),
-	TableColumn(field='gini', title='Gini Coefficient', formatter=NumberFormatter(format="0.000")),
-]
-source = ColumnDataSource(d)
-dtable = DataTable(source=source, columns=columns, width=1600)
+selectVariable = Select(title='Variable', options=['Current Salary', 'Previous Salary'])
+selectCampus = Select(title='Campus', value='All', options=['All', 'Chicago', 'Springfield', 'Urbana-Champaign', 'System'])
+selectCollege = Select(title='College')
 
-curdoc().add_root(dtable)
+def update():
+	var = 'newsalaryperfte'
+	if selectVariable.value == 'Previous Salary': var = 'cursalaryperfte'
+	campus = selectCampus.value
+	if campus == 'All': campusSelection = (salaries.campus == salaries.campus)
+	else: campusSelection = (salaries.campus == campus)
+	college = selectCollege.value
+	if college == 'All': collegeSelection = (salaries.college == salaries.college)
+	else: collegeSelection = (salaries.college == college)
+	df = salaries.loc[campusSelection & collegeSelection].groupby(
+		['campus', 'college', 'dept'])[var].agg(
+		['size', 'count', 'min', 'mean', 'max', gini]).reset_index()
+	columns = [
+		TableColumn(field='campus', title='Campus'),
+		TableColumn(field='college', title='College'),
+		TableColumn(field='dept', title='Department'),
+		TableColumn(field='size', title='# of Employees'),
+		TableColumn(field='count', title='# with Data'),
+		TableColumn(field='min', title='Min', formatter=NumberFormatter(format="$0,0")),
+		TableColumn(field='mean', title='Mean', formatter=NumberFormatter(format="$0,0")),
+		TableColumn(field='max', title='Max', formatter=NumberFormatter(format="$0,0")),
+		TableColumn(field='gini', title='Gini Coefficient', formatter=NumberFormatter(format="0.000")),
+	]
+	l.children[2] = DataTable(source=ColumnDataSource(df), columns=columns, height=650, width=1600, selectable=True, scroll_to_selection=True)
 
-# Create plot for this
+def selectCampusUpdate():
+	if selectCampus.value == 'All': selectCollege.options = ['All']
+	else: selectCollege.options = ['All'] + sorted(list(set(list(salaries.loc[salaries.campus == selectCampus.value, 'college']))))
+	selectCollege.value = 'All'
+	update()
 
+selectVariable.on_change('value', lambda attr, old, new: update())
+selectCampus.on_change('value', lambda attr, old, new: selectCampusUpdate())
+selectCollege.on_change('value', lambda attr, old, new: update())
+
+l = layout([
+	[Div(text='<font size="3"><b>Report on Salaries across Departments</b></font>', width=500)],
+	[selectVariable, selectCampus, selectCollege],
+	[DataTable()],
+	[Div(text='Data on salaries per 1.0 FTE are shown to allow comparisons for employees not on a standard full-time appointment.', width=1000)],
+])
+selectCampusUpdate()
+curdoc().add_root(l)
 
