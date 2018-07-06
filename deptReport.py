760cc4e9fd1354727cde280dce22ccc1c7195182
jtleider <jtleider@gmail.com>
2018-07-06 12:20:45 -0500

    Add line for median to report by department

--- deptReport.py
+++ deptReport.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, Span, Label
 from bokeh.models.widgets import Select, Div, Slider
 from bokeh.layouts import row, column
 from bokeh.io import curdoc
@@ -35,16 +35,18 @@ def selection():
 	df['Rank'] = df['value'].rank(ascending=False)
 	df['ylabel'] = df.apply(lambda row: '{:g} {}'.format(row['Rank'], row['empname']), axis=1)
 	df['value_scaled'] = df['value']/1000
-	return df.sort_values('value', na_position='first', ascending=True).iloc[exclude]
+	return df.sort_values('value', na_position='first', ascending=True).iloc[exclude], df['value_scaled'].quantile(0.5)
 
 def update():
-	df = selection()
+	df, median = selection()
 	source = ColumnDataSource(df)
 	newfigure = figure(plot_width=1200, plot_height=60+len(df)*15, y_range=df['ylabel'], x_axis_label='Salary in 1000s', tooltips=TOOLTIPS, tools='hover,save')
 	newfigure.title.text_font_size = '20pt'
 	newfigure.xaxis.axis_label_text_font_size = '16pt'
 	newfigure.yaxis.major_label_text_font_size = '11pt'
 	newfigure.hbar(y='ylabel', height=0.9, right='value_scaled', source=source)
+	newfigure.add_layout(Span(location=median, dimension='height', line_dash='dashed', line_width=3))
+	newfigure.add_layout(Label(x=median+2, y=(len(df)-1)/2, text='Median (no exclusions)'))
 	layout.children[1] = newfigure
 
 def selectCampusUpdate():
